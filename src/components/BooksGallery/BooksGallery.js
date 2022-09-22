3bc8227605d03f0126aff4768c22496db00060e8
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-09-21 21:02:51 -0500

    setting up booksgallery

--- src/components/BooksGallery/BooksGallery.js
+++ src/components/BooksGallery/BooksGallery.js
@@ -2,13 +2,49 @@ import React from 'react'
 import './BooksGallery.css'
 import BookCard from '../BookCard/BookCard'
 
-const BooksGallery = () => {
+const BooksGallery = ({bookLists}) => {
+    const bookShelves = bookLists.map((bookList) => {
+        console.log ("bookList: ", bookList.list_name.hardcover)
+        const { list_id, title, list_name } = bookList
+        return (
+            <BookCard 
+                key={Math.random()}
+                title={title}
+                genre={list_name}
+
+            />
+        )
+    } )
     return (
         <main className='bookgallery-container'>
             <h2 className="bookgallery-container-text">This is where all the books will go</h2>
-            <BookCard />
+            {bookShelves}
         </main>
     )
 }
+//author
+//book_image
+//description
+//title
+//list-name(genre)
+
+// Combined Print and E-Book Fiction
+// Combined Print and E-Book Nonfiction
+// Hardcover Fiction
+// Hardcover Nonfiction
+// Trade Fiction Paperback
+// Paperback Nonfiction
+// Advice How-To and Miscellaneous
+// Childrens Middle Grade Hardcover
+// Picture Books
+// Series Books
+// Young Adult Hardcover
+// Audio Fiction
+// Audio Nonfiction
+// Business Books
+// Graphic Books and Manga
+// Mass Market Monthly
+// Middle Grade Paperback Monthly
+// Young Adult Paperback Monthly
 
 export default BooksGallery
