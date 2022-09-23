609c051ab3bae8fb2bc8374d1573f5b1bb8685eb
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2022-09-23 00:23:22 -0500

    added styling to book displays

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import './BookCard.css'
 
-const BookCard = ({ author, title, bookCover, id, genre }) => {
+const BookCard = ({ author, title, bookCover, id, genre, rank, publisher, description }) => {
     // console.log('bookCover: ', bookCover)
     return (
-        <div className="book-card">
-            <img className="bookCover-container" src={bookCover} alt="book cover"/>
-            <h3>{genre}</h3>
-            <h3>{title}</h3>
-            <h3>{author}</h3>
-        </div>
+        <article className='book-card-container'>
+            <div className="book-card">
+                <img className="bookCover-container" src={bookCover} alt='book cover'/>
+            </div>
 
+            <div className='description-box'>    
+                <p className='title'>{title}</p>
+                <p className='author'>{author}</p>
+                <p className='genra'>{genre}</p>
+                <p className='current-'>NY Times Current Rank: {rank}</p>
+                <p className='description'>{description}</p>
+                <p className='info-publishing'>Publisher: {publisher}</p>
+            </div>
+
+        </article>
     )
 }
 
@@ -21,3 +29,5 @@ export default BookCard
 //description
 //title
 //list-name(genre)
+
+
